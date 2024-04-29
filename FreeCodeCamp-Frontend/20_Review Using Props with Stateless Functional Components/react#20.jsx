98f36246447c439e88e132a2f35b9048540c326a
Tomas Riera <53879865+tomas26j@users.noreply.github.com>
2024-04-29 19:40:39 -0300

    Create react#20.jsx

---
+++ FreeCodeCamp-Frontend/20_Review Using Props with Stateless Functional Components/react#20.jsx
@@ -0,0 +1,33 @@
+class CampSite extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <Camper name={this.props.name} />
+      </div>
+    );
+  }
+}
+// Change code below this line
+class Camper extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <p>{this.props.name}</p>
+      </div>
+    );
+  }
+}
+
+Camper.defaultProps = {
+  name: "CamperBot",
+};
+
+Camper.propTypes = {
+  name: PropTypes.string.isRequired,
+};
